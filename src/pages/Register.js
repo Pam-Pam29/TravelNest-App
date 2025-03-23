91ef9df6f1c6b8c4854479b5e0a549a93b28419a
Victoria Fakunle <v.fakunle@alustudent.com>
2025-03-23 16:01:14 +0200

    Update Register.js

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,1 +1,14 @@
+// src/pages/Register.js
+import React from 'react';
+import RegisterComponent from '../components/auth/Register';
+
+const Register = () => {
+  return (
+    <div className="page-container">
+      <RegisterComponent />
+    </div>
+  );
+};
+
+export default Register;
 
