9127d4b930345595fcb5160b836ff3dad6771ff9
Victoria Fakunle <v.fakunle@alustudent.com>
2025-03-23 15:59:13 +0200

    Update LoadingSpinner.js

--- src/components/common/LoadingSpinner.js
+++ src/components/common/LoadingSpinner.js
@@ -1,1 +1,14 @@
+// src/components/common/LoadingSpinner.js
+import React from 'react';
+
+const LoadingSpinner = () => {
+  return (
+    <div className="loading-spinner">
+      <div className="spinner"></div>
+      <p>Loading...</p>
+    </div>
+  );
+};
+
+export default LoadingSpinner;
 
