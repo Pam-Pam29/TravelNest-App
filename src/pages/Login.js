442148c6216fe739aa248e288266d95418f08b74
Victoria Fakunle <v.fakunle@alustudent.com>
2025-03-23 16:00:53 +0200

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,1 +1,15 @@
+// src/pages/Login.js
+import React from 'react';
+import LoginComponent from '../components/auth/Login';
+
+const Login = () => {
+  return (
+    <div className="page-container">
+      <LoginComponent />
+    </div>
+  );
+};
+
+export default Login;
+
 
