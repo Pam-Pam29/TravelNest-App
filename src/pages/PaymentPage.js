e08247dbd84c86ecba6fb5ed20584e674d9229ee
Victoria Fakunle <v.fakunle@alustudent.com>
2025-03-23 16:02:33 +0200

    Update PaymentPage.js

--- src/pages/PaymentPage.js
+++ src/pages/PaymentPage.js
@@ -1,1 +1,15 @@
+// src/pages/PaymentPage.js
+import React from 'react';
+import PaymentForm from '../components/booking/PaymentForm';
+
+const PaymentPage = () => {
+  return (
+    <div className="page-container">
+      <PaymentForm />
+    </div>
+  );
+};
+
+export default PaymentPage;
+
 
