edc2949083f71e496c07285d3ffae70d5ac37135
Victoria Fakunle <v.fakunle@alustudent.com>
2025-03-23 16:01:54 +0200

    Update PackageDetailPage.js

--- src/pages/PackageDetailPage.js
+++ src/pages/PackageDetailPage.js
@@ -1,1 +1,15 @@
+// src/pages/PackageDetailPage.js
+import React from 'react';
+import PackageDetail from '../components/packages/PackageDetail';
+
+const PackageDetailPage = () => {
+  return (
+    <div className="page-container">
+      <PackageDetail />
+    </div>
+  );
+};
+
+export default PackageDetailPage;
+
 
