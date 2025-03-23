e5767cb300ff86a0e4ed4aada3f52bbff12db8ab
Victoria Fakunle <v.fakunle@alustudent.com>
2025-03-23 16:02:51 +0200

    Update ConfirmationPage.js

--- src/pages/ConfirmationPage.js
+++ src/pages/ConfirmationPage.js
@@ -1,1 +1,14 @@
+// src/pages/ConfirmationPage.js
+import React from 'react';
+import BookingConfirmation from '../components/booking/BookingConfirmation';
+
+const ConfirmationPage = () => {
+  return (
+    <div className="page-container">
+      <BookingConfirmation />
+    </div>
+  );
+};
+
+export default ConfirmationPage;
 
