da2a4c01392e26cc978ac9cc5f8ecbc224574be3
Victoria Fakunle <v.fakunle@alustudent.com>
2025-03-23 16:02:15 +0200

    Update BookingPage.js

--- src/pages/BookingPage.js
+++ src/pages/BookingPage.js
@@ -1,1 +1,14 @@
+// src/pages/BookingPage.js
+import React from 'react';
+import BookingForm from '../components/booking/BookingForm';
+
+const BookingPage = () => {
+  return (
+    <div className="page-container">
+      <BookingForm />
+    </div>
+  );
+};
+
+export default BookingPage;
 
