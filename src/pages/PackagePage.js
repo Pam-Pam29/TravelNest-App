544eed86c7495e36f681f521b6c0da71bc9194c8
Victoria Fakunle <v.fakunle@alustudent.com>
2025-03-23 16:01:36 +0200

    Update PackagePage.js

--- src/pages/PackagePage.js
+++ src/pages/PackagePage.js
@@ -1,1 +1,14 @@
+// src/pages/PackagesPage.js
+import React from 'react';
+import PackageList from '../components/packages/PackageList';
+
+const PackagesPage = () => {
+  return (
+    <div className="page-container">
+      <PackageList />
+    </div>
+  );
+};
+
+export default PackagesPage;
 
